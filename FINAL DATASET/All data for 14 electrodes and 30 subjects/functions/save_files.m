23518b1a8ba66fd512a8befb2bc39410dad1ca41
nimanuel <132136972+nimanuel@users.noreply.github.com>
2023-08-26 17:41:35 +0300

    26.8 v2

--- FINAL DATASET/All data for 14 electrodes and 30 subjects/functions/save_files.m
+++ FINAL DATASET/All data for 14 electrodes and 30 subjects/functions/save_files.m
@@ -1,15 +1,23 @@
 % orgenizes the data in .mat tables for easy usedata_table_form
+folderName = "..\data_table_form";  
+
+% Check if the folder exists
+if ~exist(folderName, 'dir')
+    % If the folder doesn't exist, create it
+    mkdir(folderName);
+end
+
+
 % lying probe
-mkdir("..\data_table_form");
 filepath = '..\data_table_form\lying_probe.mat';
 save(filepath);
 processFiles("..\lying", filepath, 'probe.txt')
-
+%%
 % lying target
 filepath = '..\data_table_form\lying_target.mat';
 save(filepath);
 processFiles("..\lying", filepath, 'target.txt')
-
+%%
 % lying irrelevant
 filepath = '..\data_table_form\lying_irrelevant.mat';
 save(filepath);
@@ -34,6 +42,9 @@ processFiles("..\honest", filepath, 'irrelevant.txt')
 
 % Recursive function to process files
 function []= processFiles(dir_path, save_path, stim_type)
+    f = 500; % Hz
+    event_len = 1.6; % event length
+    box_num = f*event_len; % there are 30 segments os 1.6 sec for each data row
 
     % Get a structure array containing information about the files
     files = dir(dir_path);
@@ -57,8 +68,13 @@ function []= processFiles(dir_path, save_path, stim_type)
         
             % Load the data
             data = load(file_path);
-            data = data.';
-            data_with_avg = data;
+            data = data.';            
+            numRows = size(data, 1);
+            numColumns = size(data, 2);
+            threeD_Array = reshape(data, numRows/box_num, numColumns, box_num);
+
+  
+            data_with_avg = threeD_Array;
             dir_list = strsplit(file_path, '\');
             var_name = strjoin({dir_list{3}, dir_list{4}}, '_');
             S.(var_name) = data_with_avg;
