02a8323dc79d05a792846521974db764c8db97e8
nimanuel <132136972+nimanuel@users.noreply.github.com>
2023-09-05 20:12:05 +0300

    alon

--- FINAL DATASET/All data for 14 electrodes and 30 subjects/functions/dataStructOrganizer.m
+++ FINAL DATASET/All data for 14 electrodes and 30 subjects/functions/dataStructOrganizer.m
@@ -29,14 +29,26 @@ disp(Table_for_subj);
 % disp(data_cell);
 %%
 
-
-data_cell = {};
-for subject_num = 1:SUBJECT_NUMBER  %       ????
-    data_cell{end + 1} = createSubjectArray(channel_matrix, subject_num);
-    
+data=load(lying_path_list{1});
+data_subject = {};
+data_session = {};
+for subjectNumber = 1:SUBJECT_NUMBER
+    for sessionNumber = 1:5
+        
+%         for subject_num = 1:SUBJECT_NUMBER  %       ????
+%             data_cell{end + 1} = createSubjectArray(channel_matrix, subject_num);
+            try
+                data_session{end + 1} = create_subject_table(data.("subject"+subjectNumber+"_session"+sessionNumber),elects);
+            catch
+                continue;
+            end
+        end
+        data_subject{subjectNumber} = data_session;
+        data_session = {};
+%     end
 end
-
-
+sub5ses2e1=data_subject{5}{2}(:,1,1);
+disp(1);
 
 % target = (1:10);
 % subject1.honest_target = target;
@@ -50,26 +62,27 @@ end
 
 %%
 
-temp = create_subject_table(1);
-
+temp = create_subject_table(1,elects);
 
 
 
 %%
 % add 
-
-function[sub] = createSubjectArray(channel_matrix, subject_num)
-    
-    fieldName = ['subject' num2str(subject_num)];
-    subStruct.channels = channel_matrix;
-    sub.(fieldName) = subStruct;
-
-end
-
+% 
+% function[sub] = createSubjectArray(channel_matrix, subject_num)
+%     
+%     fieldName = ['subject' num2str(subject_num)];
+%     subStruct.channels = channel_matrix;
+%     sub.(fieldName) = subStruct;
+% 
+% end
+% 
 
 
 %%
-function [subject_table] = create_subject_table(subject_num)
+function [subject_table] = create_subject_table(session,elects)
+
+constScript; % holds all the constants
 %     lying_path_list = {lying_probe_path, lying_target_path, lying_irrelevant_path};
 %     honest_path_list = {honest_probe_path, honest_target_path, honest_irrelevant_path};
 %     var_name = sub_list{:, i};
@@ -77,21 +90,18 @@ function [subject_table] = create_subject_table(subject_num)
 %     signal = eval(var_name);
 
 % lying probe
-    fieldName = ['subject' num2str(subject_num) '_session1'];
-    path = lying_path_list[1];
-    load(path,"subject1_session1");
-    signal = eval("fieldName");
-    subject_table = zeros(size(signal(1),size(signal(2),5)));
+%     fieldName = ['subject' num2str(subject_num) '_session1'];
+%     path1 = lying_path_list{1};
+%     load(path1,"subject1_session1");
+    subject_table = zeros( [size(session,1,2) 5]);
     counter =1;
     for i = 1:CHANNEL_NUMBER
         if (ismember(i, elects))
-            subject_table(:,:,counter) = signal(:,:,i);
+            subject_table(:,:,counter) = session(:,:,i);
             counter = counter + 1;
         end
     end 
 end
-
-
 
 
 
--- FINAL DATASET/All data for 14 electrodes and 30 subjects/functions/test.m
+++ FINAL DATASET/All data for 14 electrodes and 30 subjects/functions/test.m
@@ -1,10 +1,15 @@
 constScript; % holds all the constants
+elects = [electrodes.enum.Fp1.index;
+    electrodes.enum.Fp2.index;
+    electrodes.enum.P3.index;
+    electrodes.enum.P4.index];
 
 
-temp = create_subject_table(1);
+temp = create_subject_table(1,elects);
 %%
 
-function [subject_table] = create_subject_table(subject_num)
+function [subject_table] = create_subject_table(subject_num,elects)
+
 constScript; % holds all the constants
 %     lying_path_list = {lying_probe_path, lying_target_path, lying_irrelevant_path};
 %     honest_path_list = {honest_probe_path, honest_target_path, honest_irrelevant_path};
@@ -15,13 +20,12 @@ constScript; % holds all the constants
 % lying probe
     fieldName = ['subject' num2str(subject_num) '_session1'];
     path1 = lying_path_list{1};
-    load(path,"subject1_session1");
-    signal = eval("fieldName");
-    subject_table = zeros(size(signal(1),size(signal(2),5)));
+    load(path1,"subject1_session1");
+    subject_table = zeros( [size(subject1_session1,1,2) 5]);
     counter =1;
     for i = 1:CHANNEL_NUMBER
         if (ismember(i, elects))
-            subject_table(:,:,counter) = signal(:,:,i);
+            subject_table(:,:,counter) = subject1_session1(:,:,i);
             counter = counter + 1;
         end
     end 
