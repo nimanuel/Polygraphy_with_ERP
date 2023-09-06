1750a9abbb8ffa5ea2fd944cb1dabb395a5af5f8
nimanuel <132136972+nimanuel@users.noreply.github.com>
2023-09-06 15:07:48 +0300

    org data after talk w nir

--- FINAL DATASET/All data for 14 electrodes and 30 subjects/functions/dataStructOrganizer.m
+++ FINAL DATASET/All data for 14 electrodes and 30 subjects/functions/dataStructOrganizer.m
@@ -3,72 +3,59 @@
 % each subject holds a matrix of 4 cannels, and an average of the channels
 constScript; % holds all the constants
 %%
+%curr_path = lying_path_list{1};
+
+cell_arr_lying_p = createStructInner(lying_path_list{1});
+cell_arr_lying_t = createStructInner(lying_path_list{2});
+cell_arr_lying_i = createStructInner(lying_path_list{3});
+cell_arr_honest_p = createStructInner(honest_path_list{1});
+cell_arr_honest_t = createStructInner(honest_path_list{2});
+cell_arr_honest_i = createStructInner(honest_path_list{3});
+
+
+
+Data = stuct('lying_probe', cell_arr_lying_p,'lying_target', cell_arr_lying_t, ...
+    'lying_irrelevant', cell_arr_lying_i,'honest_probe', cell_arr_honest_p, ...
+    'honest_target', cell_arr_honest_t,'honest_irrelevant', cell_arr_honest_i);
+
+%%
+function [cell_arr_inner] = createStructInner(curr_path)
+% creates structs of all subjects, sessions,and itarations
+% and place them all in a cell array
+    cell_arr_inner = {};
+    [filepath,filename,ext] = fileparts(curr_path);
+    data = load(curr_path);
+    for subjectNumber = 1:SUBJECT_NUMBER
+        for sessionNumber = 1:size(data.("subject"+subjectNumber+"_session"+sessionNumber),1)
+            for repNumber = 1:size(data.("subject"+subjectNumber+"_session"+sessionNumber),2)
+                try
+                    session_tab = createSessionTable(data.("subject"+subjectNumber+"_session"+sessionNumber), repNumber);
+                    struct_inner = srtuct('sub', subjectNumber, 'sess', sessionNumber, 'rep', repNumber, 'tag', filename, 'tab', session_tab);
+                    cell_arr_inner{end+1} = struct_inner;
+                catch
+            
+                    continue;
+                end
+            end
+        end    
+    
+    end
+end
+
+%%
+
+function [subject_table] = createSessionTable(session, repNumber)
 elects = [electrodes.enum.Fp1.index;
     electrodes.enum.Fp2.index;
     electrodes.enum.P3.index;
     electrodes.enum.P4.index];
-
-% create table for one subject
-mat_sub_data = cell(5, 4);  % Assuming you want 3x3 for the example. Adjust for more columns.
-mat_sub_data{1,1} = 'Fp1';
-mat_sub_data{2,1} = 'Fp2';
-mat_sub_data{3,1} = 'P3';
-mat_sub_data{4,1} = 'P4';
-mat_sub_data{5,1} = 'average';
-Table_for_subj = cell2table(mat_sub_data, 'VariableNames', {'Electrodes', 'Probe', 'Target', 'Irrelevant'});  % Adjust 'VariableNames' for more columns.
-disp(Table_for_subj);
-% % Write the table to an Excel file
-% filename = 'dataStruct_Organizer_Table.xlsx';
-% writetable(Table_for_subj, filename);
-
-%% experiment to show Nir
-% data_cell = {};
-% for subject_num = 1:1  %       ????
-%     data_cell{end + 1} = createSubjectArray(mat_sub_data, subject_num);
-% end
-% disp(data_cell);
-%%
-
-% TODO: only for lying probe
-data = load(lying_path_list{1});
-data_subject = {};
-data_session = {};
-for subjectNumber = 1:SUBJECT_NUMBER
-    for sessionNumber = 1:5
-            try
-                data_session{end + 1} = create_subject_table(data.("subject"+subjectNumber+"_session"+sessionNumber),elects);
-            catch
-                continue;
-            end
-        end
-        data_subject{subjectNumber} = data_session.';
-        data_session = {};
-%     end
-end
-save("data_subject");
-% sub5ses2e1=data_subject{5}{2}(:,1,1);
-
-
-%%
-
-function [subject_table] = create_subject_table(session,elects)
-
 constScript; % holds all the constants
-%     lying_path_list = {lying_probe_path, lying_target_path, lying_irrelevant_path};
-%     honest_path_list = {honest_probe_path, honest_target_path, honest_irrelevant_path};
-%     var_name = sub_list{:, i};
-%     load(guilty_path, var_name);
-%     signal = eval(var_name);
-
-% lying probe
-%     fieldName = ['subject' num2str(subject_num) '_session1'];
-%     path1 = lying_path_list{1};
-%     load(path1,"subject1_session1");
-    subject_table = zeros( [size(session,1,2) 5] );
+%                               800 x 5
+    subject_table = zeros( [size(session,1) 5] );
     counter =1;
     for i = 1:CHANNEL_NUMBER
         if (ismember(i, elects))
-            subject_table(:,:,counter) = session(:,:,i);
+            subject_table(:,counter) = session(:,repNumber,i);
             counter = counter + 1;
         end
     end 
