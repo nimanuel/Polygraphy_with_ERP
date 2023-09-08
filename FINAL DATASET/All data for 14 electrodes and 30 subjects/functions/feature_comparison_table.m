8ec2314006a4f33021942e8783b25dd072118595
mayanamer <104129936+mayanamer@users.noreply.github.com>
2023-09-08 17:38:24 +0300

    Update feature_comparison_table.m

--- FINAL DATASET/All data for 14 electrodes and 30 subjects/functions/feature_comparison_table.m
+++ FINAL DATASET/All data for 14 electrodes and 30 subjects/functions/feature_comparison_table.m
@@ -1,41 +1,25 @@
 % feature_comparison_table
 % this table will help us eveluate the features 
 
-
-% Assuming you want 3x3 for the example. Adjust for more columns.
-% SIGNAL_NUM = 3;
 FEATURE_NUM = 8;
-
-feat_comp_table = cell(2, FEATURE_NUM+2); 
-
-% 
-% feat_comp_table{1,1} = 'signal1';
-% feat_comp_table{2,1} = 'signal2';
-% feat_comp_table{3,1} = 'signal3';
-% for i = 1:SIGNAL_NUM
-%     feature_vector = extractFeaturs(data_subject{5}{2}(:,1,i));
-% 
-%     for j = 2:(FEATURE_NUM+1)
-%         feat_comp_table{i,j} = feature_vector(j-1);
-% 
-%     end
-% end    
-% 
+feat_comp_table = cell(0, FEATURE_NUM+2); 
 
 fields = fieldnames(final_data);
 for i = 1:numel(fields)
     field = fields{i};
     for j =1:size(final_data.(field),2)
-        feature_vector = extractFeaturs(final_data.(field){1,j}.tab(5,:));
         if final_data.(field){1,j}.sub > 2  % only 2 subjects for now. delete "if" later
             continue;
+        elseif final_data.(field){1,j}.rep > 30
+            continue;
         end
-        feat_comp_table{j,1} = structGetName(final_data.(field){1,j});
+        feature_vector = extractFeaturs(final_data.(field){1,j}.tab(5,:));
+        feat_comp_table{end+1,1} = structGetName(final_data.(field){1,j});
         for t = 2:(FEATURE_NUM+1)
-            feat_comp_table{j,t} = feature_vector(t-1);
+            feat_comp_table{end,t} = feature_vector(t-1);
            
         end
-       feat_comp_table{j,FEATURE_NUM+2} = structGetTag(final_data.(field){1,j});
+       feat_comp_table{end,FEATURE_NUM+2} = structGetTag(final_data.(field){1,j});
  
     end
 end
