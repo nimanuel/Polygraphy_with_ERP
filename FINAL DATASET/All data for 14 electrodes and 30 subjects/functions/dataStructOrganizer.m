7b3de3b3faaa32ac2ce605d516770322d0e46f21
mayanamer <104129936+mayanamer@users.noreply.github.com>
2023-09-09 11:19:28 +0300

    subtract Pz (Fz actually)

--- FINAL DATASET/All data for 14 electrodes and 30 subjects/functions/dataStructOrganizer.m
+++ FINAL DATASET/All data for 14 electrodes and 30 subjects/functions/dataStructOrganizer.m
@@ -1,27 +1,27 @@
-% % dataStructOrganizer
-% % orgenizes the data in cell arrays of subjects
-% % each subject holds 
-% % 
-% % 
-% % a matrix of 4 cannels, and an average of the channels
-% constScript; % holds all the constants
-% %%
-% %curr_path = lying_path_list{1};
-% 
-% cell_arr_lying_p = createStructInner(lying_path_list{1});
-% cell_arr_lying_t = createStructInner(lying_path_list{2});
-% cell_arr_lying_i = createStructInner(lying_path_list{3});
-% cell_arr_honest_p = createStructInner(honest_path_list{1});
-% cell_arr_honest_t = createStructInner(honest_path_list{2});
-% cell_arr_honest_i = createStructInner(honest_path_list{3});
+% dataStructOrganizer
+% orgenizes the data in cell arrays of subjects
+% each subject holds 
 % 
 % 
-% %% 
-% 
-% final_data = struct('lying_probe', {cell_arr_lying_p},'lying_target', {cell_arr_lying_t}, ...
-%     'lying_irrelevant', {cell_arr_lying_i},'honest_probe', {cell_arr_honest_p}, ...
-%     'honest_target', {cell_arr_honest_t},'honest_irrelevant', {cell_arr_honest_i});
-% save("final_data");
+% a matrix of 4 cannels, and an average of the channels
+constScript; % holds all the constants
+%%
+%curr_path = lying_path_list{1};
+
+cell_arr_lying_p = createStructInner(lying_path_list{1});
+cell_arr_lying_t = createStructInner(lying_path_list{2});
+cell_arr_lying_i = createStructInner(lying_path_list{3});
+cell_arr_honest_p = createStructInner(honest_path_list{1});
+cell_arr_honest_t = createStructInner(honest_path_list{2});
+cell_arr_honest_i = createStructInner(honest_path_list{3});
+
+
+%% 
+
+final_data = struct('lying_probe', {cell_arr_lying_p},'lying_target', {cell_arr_lying_t}, ...
+    'lying_irrelevant', {cell_arr_lying_i},'honest_probe', {cell_arr_honest_p}, ...
+    'honest_target', {cell_arr_honest_t},'honest_irrelevant', {cell_arr_honest_i});
+save("final_data");
 
 %% try to use final data
 
@@ -67,12 +67,13 @@ function [subject_table] = createSessionTable(session, repNumber)
         electrodes.enum.P3.index;
         electrodes.enum.P4.index];
     constScript; % holds all the constants
+    ground = electrodes.enum.Fz.index;
     %                               800 x 5
     subject_table = zeros( [size(session,1) 5] );
     counter =1;
     for i = 1:CHANNEL_NUMBER
         if (ismember(i, elects))
-            subject_table(:,counter) = session(:,repNumber,i);
+            subject_table(:,counter) = session(:,repNumber,i) - session(:,repNumber,ground);
             counter = counter + 1;
         end
     end 
